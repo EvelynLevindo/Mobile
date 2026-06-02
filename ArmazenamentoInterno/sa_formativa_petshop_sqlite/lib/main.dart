2756d78b113db75a0c24ca920d436bb227cda736
EvelynLevindo <levindoevelyn2@gmail.com>
2026-06-02 17:12:40 -0300

    ...

---
+++ ArmazenamentoInterno/sa_formativa_petshop_sqlite/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+void main(List<String> args) {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(
+    MaterialApp(
+      title: "PetShop SqLite",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.deepOrange),
+      home: HomeScreen(),
+    ),
+  );
+}
